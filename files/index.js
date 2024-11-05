626e48ba40e5358ddfd468b5eb7adacfed460005
sdgn230hj <sdgn230hj@protonmail.com>
2024-11-05 03:46:02 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '827b5d4f-05d7-4989-ab4a-b8fd5a37cf8d'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo3.cchh3.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZmU5NmNiZDA0MjNkYTcyMjQwNDZjOThiNDY5MWMyZWUiLCJ0IjoiZGVkYWJmODEtZDdjZC00ZDE4LWEzNzgtMTI3NTUwYWZjMmJmIiwicyI6Ik1EWmpZV1ptWmpVdFpqSTJZaTAwT0RCbExUazNOemd0WXpNNU5EQXhabVZrWVRaayJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo3.chr003.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZmU5NmNiZDA0MjNkYTcyMjQwNDZjOThiNDY5MWMyZWUiLCJ0IjoiYjNmMWVmMWMtOTcyYy00MmYxLWFhYzYtMjQ3MDMwNGJmNWQzIiwicyI6Ik16UTJNelpsT0RBdE16ZGtZaTAwWkRSbUxXRmxaamt0WmpWbFpUQXpNakUxTnpObSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
